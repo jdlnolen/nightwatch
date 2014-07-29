869d23bc4e9bae1a03ece53ddac09978bd3907eb
JDL Nolen <jdlnolen@gmail.com>
2014-07-29 16:01:09 +0000

    first commit

---
+++ tests/helloworld.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "Hello World" : function (client) {
+    client
+      .url("http://127.0.0.1:3000")
+      .waitForElementVisible("body", 1000)
+      .assert.title("nightwatch")
+      .end();
+  }
+};
+
