083c0c501f944ee81b45f7d0d05e5427791bdb09
vagrant <vagrant@precise64.(none)>
2014-07-31 10:24:44 +0000

    tweaked help

--- help.sh
+++ help.sh
@@ -2,4 +2,4 @@ export TPATH=$(pwd)"/.meteor/local/build"
 ## cd "$var1"
 ## npm install nightwatch@0.5.3
 npm install --prefix $TPATH nightwatch@0.5.3
-
+$TPATH/node_modules/nightwatch/bin/nightwatch -c packages/selenium-nightwatch/nightwatch_from_app_root.json $1 $2
